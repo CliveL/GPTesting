d398cc069b469132e9123015854d0cfb1f36cf7f
CliveL <lovett.clive@gmail.com>
2014-10-02 15:20:28 +0100

    Build Upload 4: Procedural Tree Search added (idtests.py, listnode.py) admin adjusted for buyside model

--- buyside/urls.py
+++ buyside/urls.py
@@ -4,12 +4,16 @@ from django.conf.urls import patterns, url
 from buyside import views
 
 urlpatterns = patterns('',
-                       # ex: /buyside/search/
-                       url(r'^search/$', views.search, name='search'),
+                       # ex: /buyside/VAGABC123456789/
                        # ex: /buyside/homepage.html
                        url(r'^homepage/$', views.homepage, name='homepage'),
                        # ex: /buyside/vehicleconfirm/
                        url(r'^vehicleconfirm/$', views.vehicleconfirm, name="vehicleconfirm"),
                        # ex: /buyside/vehicleconfirm/
                        url(r'^treelist/$', views.treelist, name="treelist"),
+                       # this is the entry point if someone has Googled a specific part
+                       url(r'^(?P<search_id_1>\w+)/$', views.search, name='search'),
+                       # add search if someone has a specific car, i.e. the have come from homepage
+                       # url may be copy and pasted to be sent in an e-mail
+                       url(r'^(?P<search_id_1>\w+)/(?P<search_id_2>\w+)/$', views.search, name='search2'),
 )
--- geckoparts/settings.py
+++ geckoparts/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'geckoparts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'geckotest',
+        'USER': 'djangogecko',
+        'PASSWORD': 'gecko',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
